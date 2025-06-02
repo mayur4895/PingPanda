4835f6b5cf091354edc031f414fd63bef09f0625
mayur shinde <101582238+mayur4895@users.noreply.github.com>
2025-06-02 22:24:47 +0530

    "last commit"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ const eb_garamond = EB_Garamond({
 })
 
 export const metadata: Metadata = {
-  title: "jStack App",
-  description: "Created using jStack",
+  title: "Pingpanda",
+  description: "Created by mayur",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 }
 
